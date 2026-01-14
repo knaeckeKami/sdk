2f9106d6c819cde8ada0dcca3d16c339b4dc598c
Martin Kamleithner <martin.kamleithner@gmail.com>
2026-01-14 17:50:27 +0000

    [linter] Check constructor params for type_annotate_public_apis

--- pkg/linter/test/rules/type_annotate_public_apis_test.dart
+++ pkg/linter/test/rules/type_annotate_public_apis_test.dart
@@ -7,8 +7,7 @@ import 'package:test_reflective_loader/test_reflective_loader.dart';
 import '../rule_test_support.dart';
 
 void main() {
-  // TODO(srawlins): Add tests with constructor parameters, enums, unnamed
-  // extensions.
+  // TODO(srawlins): Add tests with enums, unnamed extensions.
   defineReflectiveSuite(() {
     defineReflectiveTests(TypeAnnotatePublicApisTest);
   });
@@ -243,6 +242,25 @@ extension E on int {
 ''',
       [lint(27, 1)],
     );
+  }
+
+  test_instanceConstructor_parameterMissingType() async {
+    await assertDiagnostics(
+      r'''
+class A {
+  A(p);
+}
+''',
+      [lint(14, 1)],
+    );
+  }
+
+  test_instanceConstructor_privateNamed_parameterMissingType() async {
+    await assertNoDiagnostics(r'''
+class A {
+  A._(p);
+}
+''');
   }
 
   test_instanceMethod_onClass_noReturnType() async {
